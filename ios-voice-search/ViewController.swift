b2e98dbf2ee3b4c1de33e2d1a67685848cb94475
Александр Пономарев <alex.ponomarev@MacBook-Pro-Aleksandr.local>
2017-07-05 17:20:26 +0300

    Init proj

---
+++ ios-voice-search/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-voice-search
+//
+//  Created by Александр Пономарев on 05.07.17.
+//  Copyright © 2017 Base team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
